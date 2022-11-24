44e4febd28ca43af4d41b28a6ad030ae20de7e63
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2022-11-23 20:59:02 -0800

    Make state flatter and update App.js methods.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -28,34 +28,36 @@ class App extends React.Component {
           id: 3
         }
       ],
-      playlist: {
-        playlistName: "My New Playlist",
-        tracks: [
-          {
-            name: "Playlist Track 2",
-            artist: "Playlist Artist 2",
-            album: "Playlist Album 2",
-            id: 5
-          }
-        ]
-      }
+      playlistName: "My New Playlist",
+      playlistTracks: [
+        {
+          name: "Playlist Track 2",
+          artist: "Playlist Artist 2",
+          album: "Playlist Album 2",
+          id: 5
+        }
+      ]
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
-  addTrack(newTrack) { //Test and remove console.logs if it works
-    const playlistHasTrack = this.state.playlist.tracks.findIndex(track => track.id === newTrack.id) !== -1;
+  updatePlaylistName(newName) {
+    this.setState({ playlistName: newName });
+  }
+  addTrack(newTrack) {
+    const playlistHasTrack = this.state.playlistTracks.findIndex(track => track.id === newTrack.id) !== -1;
 
     if (!playlistHasTrack) {
       this.setState((prevState) => (
-        { playlist: { tracks: [...prevState.playlist.tracks, newTrack] } }
+        { playlistTracks: [...prevState.playlistTracks, newTrack] }
         ));
     };
   }
   removeTrack(trackToRemove) {
-    const newTrackList = this.state.playlist.tracks.filter((track) => track.id !== trackToRemove.id)
+    const newTrackList = this.state.playlistTracks.filter((track) => track.id !== trackToRemove.id)
 
-    this.setState({ playlist: { tracks: [...newTrackList] } })
+    this.setState({ playlistTracks: [...newTrackList] })
   }
   render() {
     return (
@@ -65,7 +67,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlist.playlistName} playlistTracks={this.state.playlist.tracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} onNameChange={this.updatePlaylistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
